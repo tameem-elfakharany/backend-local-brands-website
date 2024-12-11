ab26cfcad1b8b1dc82e96e34777cc6f179de379d
tameem-elfakharany <145559834+tameem-elfakharany@users.noreply.github.com>
2024-12-11 04:35:59 +0200

    db

---
+++ db.js
@@ -0,0 +1,25 @@
+const sqlite3= require('sqlite3').verbose();
+const db = new sqlite3.Database('./database.db');
+
+const CreateUsertable=`CREATE TABLE IF NOT EXISTS user(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT NOT NULL,
+email TEXT UNIQUE NOT NULL,
+username TEXT NOT NULL,
+password TEXT NOT NULL,
+phonenumber TEXT 
+)`;
+
+
+db.serialize(()=>{
+    db.run(CreateUsertable,(err)=>{
+        if (err){
+            console.error("failed to create user table",err)
+
+        }
+        else {console.log("UserTable created successfully")
+
+        }
+    })
+})
+module.exports ={db,CreateUsertable}
