3f0ff258ce9e513d0736a3022dd7e917cd609024
tameem-elfakharany <145559834+tameem-elfakharany@users.noreply.github.com>
2024-12-12 20:50:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ server.post('/user/register', (req , res)=> {
     let username = req.body.username;
     let phonenumber = req.body.phonenumber;
 
-    if (!name || !email || !password || !username|| !phonenumber){
-        return res.status(400).send('registration is required');
+    if (!name || !email || !password || !username){
+        return res.status(400).send('registration is incomplete');
     }
     const insertquery = `INSERT INTO user(name, email, password, username, phonenumber)Values('${name}', '${email}', '${password}', '${username}', '${phonenumber}')`;
     db.run(insertquery, (err) =>{
@@ -66,8 +66,55 @@ server.get('/users', (req, res) => {
     });
 });
 
+server.delete('/user/account/delete/:id', (req, res)=>{
+    let usersid= parseInt(req.params.id,10)
+    const deleting_account= `DELETE FROM user WHERE id =${usersid}`
+    db.run(deleting_account, (err) =>{
+        if (err){
+            return res.status(500).send("account cannot be deleted")
 
+        }else {
+            return res.status(200).send("account deleted successfully")
+        }
+    })
 
+})
+
+server.get('/allbrands', (req, res) => {
+    const getAllbrandsQuery = `SELECT * FROM brand` ;
+
+    db.all(getAllbrandsQuery, [], (err, rows) => {
+        if (err) {
+            console.error("Database error:", err);
+            return res.status(500).send("An error occurred.");
+        }
+
+        if (rows.length === 0) {
+            return res.status(404).send("No brands found.");
+        } else {
+            return res.status(200).json(rows); 
+        }
+    });
+});
+
+server.post(`/brands/addbrand`, (req, res) => {
+    let name = req.body.name
+    let description = req.body.description
+    let location = req.body.location
+    let rating = req.body.rating
+    let query = `INSERT INTO brand (name,description,location,rating) 
+    VALUES ('${name}', '${description}', '${location}', '${rating}')`;
+    db.run(query, (err) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).send(err)
+        }
+        else {
+            return res.status(200).send(`brand added successfully`)
+        }
+    })
+
+})
 
 
 
